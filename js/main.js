ec22691542296b06d6f2c00f076a35a438841249
zautomata <zautomata@gmail.com>
2018-09-16 22:57:08 -0400

    forntend hacknight

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*** start of autosuggest ***/
+
+var options = {
+	url: "./DATA/fruitsAndVegetables.json",
+
+	categories: [
+		{   //Category fruits
+			listLocation: "fruits",
+			header: "-- Fruits --"
+		}, 
+		{   //Category vegetables
+			listLocation: "vegetables",
+			header: "-- Vegetables --"
+		}
+	]
+
+};
+
+$("#categories-basic").easyAutocomplete(options);
+/*** end of autosuggest ***/
+
+/*** start of map ***/
+
+//var map = L.map('map').setView([-41.2858, 174.78682], 14);
+var map = L.map('map').setView([31.205753, 29.924526], 14);
+//var map = L.map('map').setView([30.2, 31], 8);
+mapLink = 
+	'<a href="https://www.citybox.io/copyright">citybox.io</a>';
+wholink = 
+	'';
+L.tileLayer(
+	'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		attribution: '&copy; '+mapLink+', '+wholink,
+		maxZoom: 18,
+	}).addTo(map);
+// 
+// mapLink = 
+// '<a href="http://www.esri.com/">Esri</a>';
+// wholink = 
+// 	'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+
+/*** end of map ***/
